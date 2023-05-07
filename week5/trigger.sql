693864459be94b4ef73043ce258b403875447acf
JaviPatron <jpatronmf@gmail.com>
2023-05-07 14:27:42 -0700

    trigger example

---
+++ week5/trigger.sql
@@ -0,0 +1,14 @@
+CREATE TRIGGER update_species
+AFTER INSERT ON new_species
+FOR EACH ROW
+BEGIN
+   UPDATE new_species
+   SET Scientific_name = NULL
+   WHERE Code = new.Code | Scientific_name = '';
+END;
+
+
+
+
+
+
